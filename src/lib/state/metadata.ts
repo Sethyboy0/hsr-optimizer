d7d6e69334d184288645818bb0053b557d99dffa
Sethyboy0 <sethyboy0@hotmail.com>
2025-03-03 20:32:24 -0500

    Add glamoth to Lingsha

--- src/lib/state/metadata.ts
+++ src/lib/state/metadata.ts
@@ -4642,6 +4642,7 @@ function getScoringMetadata(): Record<string, ScoringMetadata> {
           Sets.TheWondrousBananAmusementPark,
           Sets.TaliaKingdomOfBanditry,
           Sets.DuranDynastyOfRunningWolves,
+          Sets.FirmamentFrontlineGlamoth,
           Sets.InertSalsotto,
           ...SPREAD_ORNAMENTS_2P_SUPPORT,
         ],
